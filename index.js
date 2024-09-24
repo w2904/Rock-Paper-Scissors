190e2951c263566bcab1c6fd4d07f25c153c1de0
w2904 <153371554+w2904@users.noreply.github.com>
2024-09-24 23:37:27 +0000

    Change JS to work without reloads
    HTML changes:
    Add submit button so the choice of computer won't display prematurely.
    Add reset button and display choices in <p>.
    Change from "human" to "player".
    
    CSS changes:
    Change from displaying glowing border to instead use :focus.
    WIP: switch from px to rem and viewport units.
    
    JS changes:
    Remove unnecessary variables.
    Merge display and getComputerChoice to shorten amount of code.
    Replace input field with paragraph after submitting.
    Make playRound more conscise.
    Make playGameCicle for multiple rounds without reloading a page.
    Add reset, clear localStorage upon reload.
    Add removeComputerStyle, so glowing-border won't be displayed upon
    multiple images.

--- index.js
+++ index.js
@@ -1,178 +1,191 @@
-const username = localStorage.getItem("username") || "Human";
+let username = "Bipedal humanoid";
 
 const usernameForm = document.querySelector("#username-form");
 
 usernameForm.addEventListener("submit", (event) => {
     const input = document.querySelector("#username");
     if (input.value !== "") {
-        localStorage.setItem("username", input.value);
+        username = input.value;
+        changeInputToPara(input, username);
+        event.preventDefault();
     }
-    return false;
 });
 
-const getHumanChoice = document.querySelector(".rps-choices.human");
+function changeInputToPara(input, username) {
+    const inputLabel = document.querySelector("label[for='username']");
+    input.style.display = "none";
+    inputLabel.style.display = "none";
+    const para = document.createElement("p");
+    para.textContent = "USERNAME: " + username;
+    usernameForm.appendChild(para);
+}
 
-function waitForHuman() {
+const getPlayerChoice = document.querySelector(".rps-choices.player");
+const confirmationButton = document.querySelector("#play-button");
+const displayPlayerChoice = document.querySelector(".choice-display.pc");
+const displayComputerChoice = document.querySelector(".choice-display.npc");
+
+function waitForPlayer() {
+    let choice;
+    getPlayerChoice.addEventListener("click", (event) => {
+        choice = event.target.id.slice(3);
+        if (choice) {
+            displayPlayerChoice.textContent = "<" + choice.toUpperCase() + ">";
+            localStorage.setItem("player", choice);
+        }
+    });
     return new Promise((resolve) => {
-        getHumanChoice.addEventListener("click", (event) => {
-            const choice = event.target;
-            if (choice.id) {
-                localStorage.setItem("human", choice.id);
+        function handleListeners() {
+            if (choice === "rock" || choice === "paper" || choice === "scissors") {
+                getComputerChoice();
+                confirmationButton.removeEventListener("click", handleListeners);
                 resolve();
             }
-        },  {
-                once: true
-            });
+            else {
+                alert("Please, click on rock, paper or scissors before confirming");
+            }
+        }
+        confirmationButton.addEventListener("click", (handleListeners));
     });
 }
 
-    const humanRock = document.querySelector("#pc-rock");
-    const humanPaper = document.querySelector("#pc-paper");
-    const humanScissors = document.querySelector("#pc-scissors");
     const computerRock = document.querySelector("#npc-rock");
     const computerPaper = document.querySelector("#npc-paper");
     const computerScissors = document.querySelector("#npc-scissors");
-    displayHumanChoice();
-
-function displayHumanChoice() {
-    const choiceID = localStorage.getItem("human");
-    switch (choiceID) {
-        case "pc-rock":
-            humanRock.classList.toggle("glowing-border");
-            getComputerChoice();
-            return;
-        case "pc-paper":
-            humanPaper.classList.toggle("glowing-border");
-            getComputerChoice();
-            return;
-        case "pc-scissors":
-            humanScissors.classList.toggle("glowing-border");
-            getComputerChoice();
-            return;
-    }
-}
 
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = Math.floor(Math.random() * 100);
     let computerChoice = "";
     if (randomNumber <= 32) {
-        computerChoice = "npc-rock";
+        computerChoice = "rock";
+        computerRock.classList.toggle("glowing-border");
     }
     else if (randomNumber <= 66) {
-        computerChoice = "npc-paper";
+        computerChoice = "paper";
+        computerPaper.classList.toggle("glowing-border");
     }
     else {
-        computerChoice = "npc-scissors";
+        computerChoice = "scissors";
+        computerScissors.classList.toggle("glowing-border");
     }
+    if (roundNumber > 1) {
+        removeComputerStyle();
+    }
+    displayComputerChoice.textContent = "<" + computerChoice.toUpperCase() + ">";
     localStorage.setItem("computer", computerChoice);
-    displayComputerChoice();
 }
 
-function displayComputerChoice() {
-    const choiceID = localStorage.getItem("computer");
-    switch (choiceID) {
-        case "npc-rock":
+function removeComputerStyle() {
+    switch (localStorage.getItem("computer")) {
+        case "rock":
             computerRock.classList.toggle("glowing-border");
-            return;
-        case "npc-paper":
+            break;
+        case "paper":
             computerPaper.classList.toggle("glowing-border");
-            return;
-        case "npc-scissors":
+            break;
+        case "scissors":
             computerScissors.classList.toggle("glowing-border");
-            return;
+            break;
     }
 }
-if (localStorage.getItem("humanPoints") === null) {
-    localStorage.setItem("humanPoints", "0");
+
+if (localStorage.getItem("playerPoints") === null) {
+    localStorage.setItem("playerPoints", "0");
 }
+
 else if (localStorage.getItem("computerPoints") === null) {
-    localStorage.setIteim("computerPoints", "0");
+    localStorage.setItem("computerPoints", "0");
 }
-const humanPoints = Number(localStorage.getItem("humanPoints")) || 0;
-const computerPoints = Number(localStorage.getItem("computerPoints")) || 0;
+
+let playerPoints = 0;
+let computerPoints = 0;
+let roundNumber = 1;
 
 function playRound() {
-    const humanChoice = localStorage.getItem("human");
+    const playerChoice = localStorage.getItem("player");
     const computerChoice = localStorage.getItem("computer");
-
-    if ((humanChoice === "pc-rock" && computerChoice === "npc-scissors") ||
-        (humanChoice === "pc-paper" && computerChoice === "npc-rock") || 
-        (humanChoice === "pc-scissors" && computerChoice === "npc-paper")) {
-            localStorage.setItem("humanPoints", String(humanPoints + 1));
-            return "humanWon";
-        }
-    else if ((computerChoice === "npc-rock" && humanChoice === "pc-scissors") ||
-            (computerChoice === "npc-paper" && humanChoice === "pc-rock") || 
-            (computerChoice === "npc-scissors" && humanChoice === "pc-paper")) {
-                localStorage.setItem("computerPoints", String(computerPoints + 1));
-                return "computerWon";
-            }
-    else {
+    const strength = {
+        "rock": { strongerThan: "scissors" },
+        "paper": { strongerThan: "rock" },
+        "scissors": { strongerThan: "paper" }
+    }
+    if (strength[playerChoice].strongerThan === computerChoice) {
+        playerPoints += 1;
+        localStorage.setItem("result", "Humanity wins Round " + roundNumber + ", but will this continue forever?");
+        return "playerWon";
+    }
+    else if (strength[computerChoice].strongerThan === playerChoice) {
+        computerPoints += 1;
+        localStorage.setItem("result", "AI wins Round " + roundNumber + " and this is a start of the end");
+        return "computerWon";
+    }
+    else if (playerChoice === computerChoice) {
+        localStorage.setItem("result", "I hope friendship wins");
         return "tie";
     }
 }
 
-const humanScore = document.querySelector("#pc-score");
-humanScore.textContent = localStorage.getItem("humanPoints") + " ← " + username + " SCORE";
-
+const playerScore = document.querySelector("#pc-score");
 const computerScore = document.querySelector("#npc-score");
-computerScore.textContent = "COMPUTER SCORE → " + localStorage.getItem("computerPoints");
-
 const result = document.querySelector("#result");
-result.textContent = localStorage.getItem("result");
-
-function displayRoundResult(resultOfRound, roundNumber) {
-    if (resultOfRound === "humanWon") {
-        localStorage.setItem("result", "Humanity wins Round " + roundNumber + ", but will this continue forever?");
-        result.textContent = localStorage.getItem("result");
-        return;
-    }
-    else if (resultOfRound === "computerWon") {
-        localStorage.setItem("result", "AI wins Round " + roundNumber + " and this is a start of the end");
-        result.textContent = localStorage.getItem("result");
-        return;
-    }
-    else {
-        localStorage.setItem("result", "I hope friendship wins");
-        result.textContent = localStorage.getItem("result");
-        return;
-    }
-}
+const resetButton = document.querySelector("#reset-button");
 
 async function playGame() {
-    const countRound = Number(localStorage.getItem("roundNumber")) || 1;
-    if (countRound > 1) {
-        const resultOfRound = playRound();
-        localStorage.setItem("roundNumber", String(countRound + 1));
-        displayRoundResult(resultOfRound, countRound);
-        await waitForHuman();
-    }
-    else {
-        await waitForHuman();
-        const resultOfRound = playRound();
-        localStorage.setItem("roundNumber", String(countRound + 1));
-        displayRoundResult(resultOfRound, countRound);
-    }
+    await waitForPlayer();
+    playRound();
+    result.textContent = localStorage.getItem("result");
+    playerScore.textContent = playerPoints + " ← " + username + " SCORE";
+    computerScore.textContent = "COMPUTER SCORE → " + computerPoints;
+    roundNumber++;
+    return new Promise((resolve) => {
+        resolve();
+    });
 }
 
-if (humanPoints >= 5 || computerPoints >= 5) {
-    if (humanPoints > computerPoints) {
-        localStorage.setItem("result", "I hope friendship wins");
+async function playGameCycle() {
+
+    while (playerPoints < 5 && computerPoints < 5) {
+        await playGame();
+    }
+
+    if (playerPoints > computerPoints) {
+        localStorage.setItem("result", "AI have lost... Right?");
         result.textContent = localStorage.getItem("result");
     }
-    else if (computerPoints > humanPoints) {
-        localStorage.setItem("result", "I hope friendship wins");
+    else if (computerPoints > playerPoints) {
+        localStorage.setItem("result", "Praise our new overlords");
         result.textContent = localStorage.getItem("result");
     }
     else {
         localStorage.setItem("result", "And lived people and robots soul to soul, didn't reap any griefs, and shared joy together forever and always... Right?");
         result.textContent = localStorage.getItem("result");
     }
-    localStorage.setItem("humanPoints", "0");
-    localStorage.setItem("computerPoints", "0");
-    localStorage.setItem("roundNumber", "0");
-    result.textContent = "I'm afraid of what's going to happen soon";
+
+    confirmationButton.style.display = "none";
+    resetButton.style.display = "inline-block";
+    resetButton.addEventListener("click", async () => {
+        await reset();
+        playGameCycle();
+    })
 }
-else {
-    playGame();
+
+function reset() {
+    return new Promise((resolve) => {
+        playerPoints = 0;
+        computerPoints = 0;
+        roundNumber = 1;
+        playerScore.textContent = playerPoints + " ← " + username + " SCORE";
+        computerScore.textContent = "COMPUTER SCORE → " + computerPoints;
+        localStorage.setItem("roundNumber", "0");
+        result.textContent = "I'm afraid of what's going to happen soon";
+        resetButton.style.display = "none";
+        confirmationButton.style.display = "inline-block";
+        resolve();
+    });
 }
+
+playGameCycle();
+
+window.addEventListener("beforeunload", () => {
+    localStorage.clear();
+})
